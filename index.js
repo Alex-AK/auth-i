f0653900744c5186aa48facb62ac55c1f53dd727
Kingaa6 <Kingaa6@gmail.com>
2019-02-25 12:50:36 -0800

    Project init, add dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan());
+
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
