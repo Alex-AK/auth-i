1d3117ecbdffe33eafa36ac4c0db2990bb1b0bd3
Kingaa6 <Kingaa6@gmail.com>
2019-02-26 12:37:52 -0800

    Fix user route, confirmed working

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const server = express();
 
-const users = require('./user/user-model');
+// data imports
+const user = require('./user/user-model');
+
+// router imports
+const userRouter = require('./user/user-route');
+
+// middleware imports
 const { authentication } = require('./auth/authentication');
 
+// server extensions
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
+// apply server routes
+server.use('/api', userRouter);
+
 // get users if authorized
 server.get('/api/users', authentication, (req, res) => {
-  users
+  user
     .get()
     .then(users => res.status(200).json({ users }))
     .catch(err => console.log(err));
--- user/user-route.js
+++ user/user-route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const users = require('../user/user-model');
 const { authentication } = require('../auth/authentication');
 
 // incoming /api
 
-router.post('/api/register', (req, res) => {
+router.post('/register', (req, res) => {
   const newUser = req.body;
   let { username, password } = req.body;
 
@@ -25,7 +26,7 @@ router.post('/api/register', (req, res) => {
     .catch(err => console.log(err));
 });
 
-router.post('/api/login', (req, res) => {
+router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   if (!username || !password) {
@@ -51,3 +52,5 @@ router.get('/api/users', authentication, (req, res) => {
     .then(users => res.status(200).json({ users }))
     .catch(err => console.log(err));
 });
+
+module.exports = router;
